0e054443b1d1c0463392d72ad204cde5d1b9ea43
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-01-30 13:34:49 -0800

    Added Initial Demo Files

---
+++ model.js
@@ -0,0 +1,18 @@
+export const model = {
+    path: "./models/biker.fbx",
+    position: {
+        x: 0,
+        y: -4.1,
+        z: 0,
+    },
+    scale: {
+        x: 0.02,
+        y: 0.02,
+        z: 0.02,
+    },
+    rotation: {
+        x: 0,
+        y: 180,
+        z: 0,
+    },
+};
