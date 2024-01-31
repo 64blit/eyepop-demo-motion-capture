15b94fc76b4922cfcfe527f575b2ef2e360de3e1
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-01-30 16:22:50 -0800

    Added multiple fixes for running in a sandbox

--- model.js
+++ model.js
@@ -1,18 +1,18 @@
 export const model = {
-    path: "./models/biker.fbx",
-    position: {
-        x: 0,
-        y: -4.1,
-        z: 0,
-    },
-    scale: {
-        x: 0.02,
-        y: 0.02,
-        z: 0.02,
-    },
-    rotation: {
-        x: 0,
-        y: 180,
-        z: 0,
-    },
+  path: "./models/biker.fbx",
+  position: {
+    x: 0,
+    y: -2.8,
+    z: 0,
+  },
+  scale: {
+    x: 0.015,
+    y: 0.015,
+    z: 0.015,
+  },
+  rotation: {
+    x: 0,
+    y: 180,
+    z: 0,
+  },
 };
