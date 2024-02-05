dae4ab199c891a8a82b0cea1bffbbde92a1dd1b9
Edmund Dao <edmund0dao@gmail.com>
2024-02-05 17:32:18 +0000

    Specified the pop details needed for this demo

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 //
 // 1. Login to your EyePop.ai account
-// 2. Create a POP, then copy and paste the UUID
+// 2. Create a Person 3D Face/Body pose detection POP
 //
 // 3. Enter the UUID here
 //
-const pop_uuid = "";
+const pop_uuid = "ce5befb6fbb943178a8a495ab1b0a910";
 //
 // 4. Then, create a secrete API key and paste it here
 //
-const api_key = "";
+const api_key =
+  "AAGyt4zUsWHBlVR5-dP6pXeeZ0FBQUFBQmx3UnN5S0p1ZGw5cnBadDZBdGZ0UC1WR0hlc2NXTjBPcUgzTXV3b3BqSjR0M291WXBUOWNnajlVRXdsdmZtY2VrS0hjbUtOSzJwTmU5OW9yTzVfR1VRWV9VNEFxdjNsc2F5SzdDU2FBQjJXMFFTalU9";
